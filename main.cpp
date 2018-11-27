2d7e1a3d0cb54695d7a2eb9c1f60949139eed5eb
shubsb <shubham.patel1@uoit.net>
2018-11-27 18:15:07 -0500

    Skybox

--- main.cpp
+++ main.cpp
@@ -13,17 +13,37 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "apis/stb_image.h"
 
 #include "ShaderProgram.h"
 #include "ObjMesh.h"
+#include "trackball.hpp"
 #include "BezierCurveRoad.h"
 
-#define SCALE_FACTOR 2.0f
+#define NUM_SHADERS 4
+#define SHADER_SWITCH_DELAY 5000
+
+#define NUM_SKYBOXES 3
+#define SKYBOX_SWITCH_DELAY 20000
 
 int width, height;
 
 GLuint programId;
 GLenum positionBufferId;
+
+bool animateShaders = false;
+
+GLuint skyboxProgramId;
+
+GLuint skybox_vbo = 0;
+
+unsigned int skyboxTextures[NUM_SKYBOXES];
+unsigned int skyboxIndex;
+int lastSkyboxTime = 0;
+bool animateSkyboxes = false;
 
 
 //Road(Cubic Bezier Curve)
@@ -49,10 +69,66 @@ unsigned int numVertices;
 
 float angle = 0.0f;
 float lightOffsetY = 0.0f;
-glm::vec3 eyePosition(0, 10, 10);
+//glm::vec3 eyePosition(0, 10, 10);
 bool animateLight = true;
 bool rotateObject = true;
 
+float theta = -0.4f;
+float phi = -1.0f;
+float cameraRadius = 100.0f;
+
+static unsigned int createCubemap(std::vector<std::string> filenames);
+
+static void createSkybox(void) {
+  float skyboxPositions[] = {
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+
+    -1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+    -1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f
+  };
+  unsigned int numVertices = 36;
+
+  glGenBuffers(1, &skybox_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, skybox_vbo);
+  glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(float) * 3, skyboxPositions, GL_STATIC_DRAW);
+}
 
 static void setupRoad(){
   vertexPositionData = road.getPath();
@@ -98,6 +174,20 @@ static void createGeomentry(void) {
 static void update(void) {
     int milliseconds = glutGet(GLUT_ELAPSED_TIME);
 
+    if (animateSkyboxes) {
+     int duration = milliseconds - lastSkyboxTime;
+
+     if (duration > SKYBOX_SWITCH_DELAY) {
+        skyboxIndex++;
+
+        if (skyboxIndex >= NUM_SKYBOXES) {
+           skyboxIndex = 0;
+        }
+
+        lastSkyboxTime = milliseconds;
+     }
+   }
+
     // rotate the shape about the y-axis so that we can see the shading
     if (rotateObject) {
       float degrees = (float)milliseconds / 80.0f;
@@ -114,6 +204,8 @@ static void update(void) {
 }
 
 static void render(void) {
+  int skyboxTexture = skyboxTextures[skyboxIndex];
+
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
    // activate our shader program
@@ -169,15 +261,51 @@ static void render(void) {
    glUseProgram(programId);
 
    //road
-   glBindVertexArray(VAO[0]);
-   glDrawArrays(GL_LINE_STRIP, 0, road.getNumCurves()*road.getNumVertecies());
+   // glBindVertexArray(VAO[0]);
+   // glDrawArrays(GL_LINE_STRIP, 0, road.getNumCurves()*road.getNumVertecies());
+   //
+   // //platform
+   // glBindVertexArray(VAO[1]);
+   // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, platformEBO);
+   // glDrawElements(GL_TRIANGLES, numVertices, GL_UNSIGNED_INT, (void*)0);
 
-   //platform
-   glBindVertexArray(VAO[1]);
-   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, platformEBO);
-   glDrawElements(GL_TRIANGLES, numVertices, GL_UNSIGNED_INT, (void*)0);
+   // draw the cube map sky box
 
-	 glutSwapBuffers();
+  // provide the vertex positions to the shaders
+  GLint skyboxPositionAttribId = glGetAttribLocation(skyboxProgramId, "position");
+  glBindBuffer(GL_ARRAY_BUFFER, skybox_vbo);
+  glEnableVertexAttribArray(skyboxPositionAttribId);
+  glVertexAttribPointer(skyboxPositionAttribId, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+  // texture sampler - a reference to the texture we've previously created
+  GLuint skyboxTextureId  = glGetUniformLocation(skyboxProgramId, "u_TextureSampler");
+  glActiveTexture(GL_TEXTURE0);  // texture unit 0
+  glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTexture);
+  glUniform1i(skyboxTextureId, 0);
+
+  glUseProgram(skyboxProgramId);
+
+  glDepthMask(GL_FALSE);
+  glDisable(GL_DEPTH_TEST);
+  glFrontFace(GL_CCW);
+  glDisable(GL_CULL_FACE);
+
+  // set model-view matrix
+
+  GLuint skyboxMVMatrixId = glGetUniformLocation(skyboxProgramId, "u_MVMatrix");
+  glUniformMatrix4fv(skyboxMVMatrixId, 1, GL_FALSE, &view[0][0]);
+
+  // set projection matrix
+  GLuint skyboxProjMatrixId = glGetUniformLocation(skyboxProgramId, "u_PMatrix");
+  glUniformMatrix4fv(skyboxProjMatrixId, 1, GL_FALSE, &projection[0][0]);
+
+  glBindVertexArray(skyboxPositionAttribId);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+
+ // disable the attribute array
+  glDisableVertexAttribArray(skyboxPositionAttribId);
+
+	glutSwapBuffers();
 }
 
 static void reshape(int w, int h) {
@@ -187,11 +315,11 @@ static void reshape(int w, int h) {
    height = h;
 }
 
-static void drag(int x, int y) {
-}
-
-static void mouse(int button, int state, int x, int y) {
-}
+// static void drag(int x, int y) {
+// }
+//
+// static void mouse(int button, int state, int x, int y) {
+// }
 
 static void keyboard(unsigned char key, int x, int y) {
     std::cout << "Key pressed: " << key << std::endl;
@@ -228,6 +356,103 @@ int main(int argc, char** argv) {
   program.loadShaders("shaders/flat_vertex.glsl", "shaders/flat_fragment.glsl");
   programId = program.getProgramId();
 
+  // determine the initial camera position
+   eyePosition.x = cameraRadius * sin(theta) * cos(phi);
+   eyePosition.y = cameraRadius * sin(theta) * sin(phi);
+   eyePosition.z = cameraRadius * cos(theta);
+
+   std::vector<std::string> filenames3;
+   filenames3.push_back("IceRiver/posz.jpg");
+   filenames3.push_back("IceRiver/negz.jpg");
+   filenames3.push_back("IceRiver/posy.jpg");
+   filenames3.push_back("IceRiver/negy.jpg");
+   filenames3.push_back("IceRiver/negx.jpg");
+   filenames3.push_back("IceRiver/posx.jpg");
+
+   skyboxTextures[2] = createCubemap(filenames3);
+   skyboxIndex = 2;
+   glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+   // create the scene geometry
+   createSkybox();
+
+   // test out quaternions
+   glm::quat rot(1.0f, 0.0f, 0.0f, 0.0f);
+   float sqrtHalf = sqrt(0.5f);
+   glm::quat rotx(sqrtHalf, sqrtHalf, 0.0f, 0.0f);  // 90 degrees about x
+   glm::quat rotz(sqrtHalf, 0.0f, 0.0f, sqrtHalf);  // 90 degrees about z
+   rot *= rotx;
+   rot *= rotz;
+
+
+   ShaderProgram skyboxProgram;
+   skyboxProgram.loadShaders("shaders/skybox_vertex.glsl", "shaders/skybox_fragment.glsl");
+  	skyboxProgramId = skyboxProgram.getProgramId();
+
+   // output some basic help
+   std::cout << "Controls:" << std::endl;
+   std::cout << "\tLeft click + drag - rotate camera" << std::endl;
+   std::cout << "\tRight click + drag - zoom camera" << std::endl;
+   std::cout << "\tr - Enable/disable object auto-rotation" << std::endl;
+   std::cout << "\ts - Enable/disable shader auto-switching" << std::endl;
+   std::cout << "\tb - Enable/disable environment auto-switching" << std::endl;
+
+
   glutMainLoop();
   return 0;
+}
+
+static unsigned int createCubemap(std::vector<std::string> filenames) {
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, numChannels;
+    for (unsigned int i = 0; i < filenames.size(); i++) {
+        unsigned char *data = stbi_load(filenames[i].c_str(), &width, &height, &numChannels, 0);
+        if (data) {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                         0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        } else {
+            std::cout << "Cubemap texture failed to load at path: " << filenames[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+    glActiveTexture(GL_TEXTURE0);
+
+    return textureID;
+}
+
+static void createTexture(std::string filename) {
+  int width, height, numChannels;
+  unsigned char *bitmap = stbi_load(filename.c_str(), &width, &height, &numChannels, 4);
+
+  GLuint textureId;
+  glGenTextures(1, &textureId);
+
+  // make this texture active
+  glBindTexture(GL_TEXTURE_2D, textureId);
+
+  // specify the functions to use when shrinking/enlarging the texture image
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  // send the data to OpenGL
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, bitmap);
+
+  glBindTexture(GL_TEXTURE_2D, 0);
+  glActiveTexture(GL_TEXTURE0);
+
+  // we don't need the bitmap data any longer
+  stbi_image_free(bitmap);
 }
